573657f60f1efbf40f948f244caf38e454318d31
Nishant Kumar <nk534031@gmail.com>
2025-05-06 08:04:29 +0530

    Complete all methods and check all stage

--- db_version/video_manager_db.py
+++ db_version/video_manager_db.py
@@ -13,6 +13,38 @@ cursor.execute('''
 ''')
 
 
+def list_all_video():
+    cursor.execute("SELECT * FROM videos")
+    rows = cursor.fetchall()
+
+    if not rows:
+        print("No video found in database.")
+    else: 
+        print("\n")
+        print("*" * 60)
+
+        for row in rows:
+            video_id, name , time = row
+            print(f"ID: {video_id}, Name: {name}, Duration: {time}")
+
+        print("*" * 60)
+
+
+def add_video(name, time):
+    cursor.execute("INSERT INTO videos (name, time) VALUES (?, ?)", (name, time))
+    conn.commit()
+
+
+def update_video(video_id, name, time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (name, time, video_id))
+    conn.commit()
+
+
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
+    conn.commit()
+
+
 def main():
     
     while True:
@@ -23,7 +55,7 @@ def main():
         print("4. Delete videos")
         print("5. Exit app")
 
-        choice = input("Enter your choice: ")
+        choice = input("\n Enter your choice: ")
 
         match choice:
             case '1':
@@ -35,14 +67,14 @@ def main():
                 add_video(name, time)
 
             case '3':
-                vidoe_id = int(intput("Enter a video ID to update: "))
+                video_id = int(input("Enter a video ID to update: "))
                 name = input("Enter a video name to update: ")
                 time = input("Enter a video time to update: ")
-                update_video(vidoe_id, name, time)
+                update_video(video_id, name, time)
 
             case '4':
-                vidoe_id = int(input("Enter a video ID to delete: "))
-                delete_video(vidoe_id)
+                video_id = int(input("Enter a video ID to delete: "))
+                delete_video(video_id)
 
             case '5':
                 break
